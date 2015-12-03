32742a3ff5600a8de5193309e0aa3023d0e75b6b
Dan Kempton <kemptondan@gmail.com>
2015-12-03 15:23:38 -0500

    added about-us page

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// var ww = document.body.clientWidth;
+
+$(document).ready(function() {
+	$(".nav li a").each(function() {
+		if ($(this).next().length > 0) {
+			$(this).addClass("parent");
+		};
+	})
+	
+	$(".toggleMenu").click(function(e) {
+		e.preventDefault();
+		$(this).toggleClass("active");
+		$(".nav").toggle();
+	});
+	adjustMenu();
+});
+
+$(window).bind('resize orientationchange', function() {
+	ww = document.body.clientWidth;
+	adjustMenu();
+});
+
+var adjustMenu = function() {
+	if (ww < 768) {
+		$(".toggleMenu").css("display", "inline-block");
+		if (!$(".toggleMenu").hasClass("active")) {
+			$(".nav").hide();
+		} else {
+			$(".nav").show();
+		}
+		$(".nav li").unbind('mouseenter mouseleave');
+		$(".nav li a.parent").unbind('click').bind('click', function(e) {
+			// must be attached to anchor element to prevent bubbling
+			e.preventDefault();
+			$(this).parent("li").toggleClass("hover");
+		});
+	} 
+	else if (ww >= 768) {
+		$(".toggleMenu").css("display", "none");
+		$(".nav").show();
+		$(".nav li").removeClass("hover");
+		$(".nav li a").unbind('click');
+		$(".nav li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function() {
+		 	// must be attached to li so that mouseleave is not triggered when hover over submenu
+		 	$(this).toggleClass('hover');
+		});
+	}
+}
+
+$(window).load(function(){
+	$('img.caption').captionjs({
+	'class_name': 'captionjs', // Class name for each <figure>
+    'schema': true,        // Use schema.org markup (i.e., itemtype, itemprop)
+    'mode': 'animated',   // default | stacked | animated | hide
+    'debug_mode': false,       // Output debug info to the JS console
+    'force_dimensions': true,        // Force the dimensions in case they cannot be detected (e.g., image is not yet painted to viewport)
+    'is_responsive': false,       // Ensure the figure and image change size when in responsive layout. Requires a container to control responsiveness!
+    'inherit_styles': false  
+});
+});
